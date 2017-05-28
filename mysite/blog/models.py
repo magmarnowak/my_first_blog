1e27bae91cbc8149c49a3db25db3e07b5efc95c6
MN <magmarnowak@gmail.com>
2017-05-28 15:36:43 +0200

    Add image field

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
@@ -9,6 +10,7 @@ class Post(models.Model):
             default=timezone.now)
     published_date = models.DateTimeField(
             blank=True, null=True)
+    img_url = models.CharField(max_length=200)
 
     def publish(self):
         self.published_date = timezone.now()
